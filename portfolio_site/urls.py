256879ff81ff482acbaa8f56736e523f68745a4e
mc5rgspider <mc5rgspider@gmail.com>
2022-01-09 18:54:51 -0500

    initial

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -19,5 +19,5 @@ from quote_generator import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    #path('about/', views.about, name='about'),
 ]
--- quote_generator/views.py
+++ quote_generator/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'quote_generator/index.html')
 
-def about(request):
-    return render(request, 'quote_generator/about.html')
+#def about(request):
+#    return render(request, 'quote_generator/about.html')
