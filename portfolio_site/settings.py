5730fcf2dacd50f1dc939cd34797f054a17f38ab
mc5rgspider <mc5rgspider@gmail.com>
2022-01-10 15:59:14 -0500

    .

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,7 +118,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_DIRS = [
+    BASE_DIR / 'quote_generator' / 'static',
+    BASE_DIR / 'movie_booking' / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
