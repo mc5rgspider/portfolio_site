5cb660eb45779abb707be6027db0a0aab3e0ab9a
mc5rgspider <mc5rgspider@gmail.com>
2022-01-11 16:09:30 -0500

    .

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    #title of the project
+    title = models.CharField(max_length=50)
+    
+    #description for the project
+    description = models.CharField(max_length=200)
+    
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
+    
+    
+    def __str__(self):
+        return self.title
--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'quote_generator',
+    'portfolio',
+    'blog',
 ]
 
 MIDDLEWARE = [
