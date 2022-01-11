2fb3a5e078daf6f0ec54c7eb4a69b0c3c0118be2
mc5rgspider <mc5rgspider@gmail.com>
2022-01-11 17:00:52 -0500

    .

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    #title of the blog
+    title = models.CharField(max_length=150)
+    
+    #description for the blog
+    description = models.TextField()
+    date = models.DateField()
+    
+    
+    def __str__(self):
+        return self.title
