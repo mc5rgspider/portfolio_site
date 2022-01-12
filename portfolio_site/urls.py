031953221abe8abf4a5be053a0b0d8d83ed92bb8
mc5rgspider <mc5rgspider@gmail.com>
2022-01-12 18:50:52 -0500

    .

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -14,25 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 #from quote_generator import views
+from portfolio import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 #Added for exchange_rate app
-from exchange_rate import views
+#from exchange_rate import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', views.home, name='home'),
+    path('', views.home, name='home'),
 
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     
     #Added for exchange_rate app
-    path('exchange_rate/', views.index, name='index'),
+    #path('exchange_rate/', views.index, name='index'),
     
     #path('about/', views.about, name='about'),
+    path('blog/', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
